ce811fbd99174e104ec2eb36e8aca7c3f864dc25
radiantfish <radientfish@gmail.com>
2022-11-05 23:38:41 +0300

    142 Implementing a Sticky Navigation Bar

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -45,6 +45,33 @@ allLinks.forEach(function (link) {
 });
 
 ///////////////////////////////////////////////////////////
+// sticky navigation
+
+const sectionHero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
+
+///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
